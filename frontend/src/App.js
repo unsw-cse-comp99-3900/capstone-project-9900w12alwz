fdbfa914d10df1e5e3f0f43ee5365e33aaa73c81
Haobo Wang <z5473895@ad.unsw.edu.au>
2024-06-19 23:42:57 +1000

    Install dependencies etc.
    Install dependencies
    Docker configs
    i18n configs
    Theme files
    Components:
    Language Switcher
    Theme Switcher
    Toast Notification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import LanguageSwitcher from "./components/LanguageSwitcher";
+import MyComp from "./components/MyComp";
+import ThemeSwitcher from "./components/ThemeSwitcher";
+
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyComp></MyComp>
+      <LanguageSwitcher/>
+      <ThemeSwitcher/>
     </div>
-  );
+  )
+    ;
 }
 
 export default App;
