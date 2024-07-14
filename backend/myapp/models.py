fce57f2212eb6e753d69b6539a11a5455db1ffdd
lili-coogee <z5441928@ad.unsw.edu.au>
2024-07-14 23:04:06 +1000

    lily/update the prompts
    differentiate the system and admin prompts in code, maybe diff them by db design add prompt type in the prompt tb

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -46,7 +46,7 @@ class ChatBot:
             return base64.b64encode(image_file.read()).decode("utf-8")
 
     # get prompts from prompts database
-    def get_prompts(self):
+    def get_prompts_from_db(self):
         return [
             """
            If the user's question involves creating a Capability Map and they have not provided the number
@@ -59,22 +59,43 @@ class ChatBot:
              , so please adhere strictly to this format
             """]
 
-    def chain(self, question, image_path=None):
+    # def chain(self, question, image_path="logical_dataflow.png"):
+    def chain(self, question, image_path="logical_dataflow.png"):
         llm = self.chatmodel
+        systemMsgs = []
+        defaultPrompt = """
+        you are a EA-Assist bot aims to help enterprise architects to perform
+        modelling on enterprise elements. It may range from
+        definitions to customized entity mappings based on generic industry standards and should also
+        be capable of performing entity extraction from unstructured documents.
+        """
+        systemMsgDefault = {"type": "text", "text": f"{defaultPrompt}"}
+        systemMsgs.append(systemMsgDefault)
 
         if not image_path:
-            prompts = self.get_prompts()
-            # rule based prompt
-            systemMsgs = []
-            for prompt in prompts:
-                if prompt in question:
-                    systemMsgs.append(
-                        systemMsgs.append({"type": "text", "text": f"You are a bot that is good at {prompt}"}))
+            # prompt from rules
+            prompt_rules = f"""
+                    Based on the user's input, here are some conversational rules to follow:
+                        1. If the user's question involves creating a Capability Map and they have not provided the number of levels needed in the Capability Map and the number of capabilities required for each level, please ask the user for these details.            
+                        2. If the user's question involves generating a Capability Map task, please return the corresponding JSON and CSV content in the following format: JSON: jsoncontent |||||| CSV: csvcontent. The response content will be used to build a frontend page, so please adhere strictly to this format.
 
-            if not systemMsgs:
-                systemMsg = SystemMessage(
-                    content=systemMsgs
-                )
+                        The user's request is: {question}
+                    """
+            # system prompt
+            systemMsgs.append({"type": "text", "text": f"{prompt_rules}"})
+
+            # prompt from db
+            # prompts schema design : prompt type(default/system/admin) content
+            prompts = self.get_prompts_from_db()
+            for prompt in prompts:
+                prompt_ = f"""
+                Based on the user's input, here are some conversational rules to follow:
+                    If the user's question involves {prompt} follow the corresponding instruction.            
+                """
+                systemMsgs.append({"type": "text", "text": f"{prompt_}"})
+
+            systemMsg = SystemMessage(content=systemMsgs)
+
             huamanMsg = HumanMessage(
                 content=[
                     {"type": "text", "text": f"{question}"},
@@ -105,9 +126,16 @@ class ChatBot:
             systemMsg = SystemMessage(
                 content=[
                     {"type": "text",
-                     "text": "You are a bot that is good at converting different type of diagrams to BPMN xml format"},
+                     "text": "You are a bot that is good at converting different type of diagrams to BPMN XML format"},
                     {"type": "text",
-                     "text": "always output the bpmn xml format at the end if the task is to generate to BPMN "}
+                     "text": "output the BPMN XML format"},
+                    {"type": "text",
+                     "text": """If the output content includes XML, 
+                     format it using triple backticks and label it as XML like this:
+                        ```xml
+                        xml content
+                        ```
+                    """}
                 ]
             )
             huamanMsg = HumanMessage(
