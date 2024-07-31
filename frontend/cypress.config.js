ba3d3026153651c36934d1875aa1137f96c7e742
Haobo Wang <z5473895@ad.unsw.edu.au>
2024-07-31 17:00:17 +1000

    Install and config cypress dependencies

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,7 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      timestamp: 'mmddyyyy_HHMMss',
+      reportFilename: `report-[datetime]`
     },
   },
 
